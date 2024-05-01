e91a51da46251de64a303bf5c41706e323d4cb05
kubilayeldemir <kubi0707@gmail.com>
2024-05-01 22:31:55 +0300

    icon changed & add relative date to end of the date

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 console.log('content.js loaded');
 function processElement(element) {
-    const newElement = document.createElement('div');
-    newElement.textContent = element.title;
-    newElement.style.display = 'inline-block';
+    if (element === null) {
+        return;
+    }
+    const newElement = document.createElement('span');
+    newElement.textContent = "-" + element.title + "~";
     element.parentElement.insertBefore(newElement, element);
-    element.remove();
 }
 function processAllElements() {
     document.querySelectorAll('relative-time[tense="past"]').forEach(processElement);
